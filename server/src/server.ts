9e03b107efb3907142c8185164ded33cc3ea15c4
Sonam Sharma <sonamsharma0306@gmail.com>
2026-06-26 09:30:21 +0530

    express change

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import { createApp } from "./app.js";
 import { env } from "./config/env.js";
 
 const app = createApp();
-
+app.set("trust proxy", 1);
 const server = app.listen(env.PORT, () => {
   console.log(`PDFPilot API listening on port ${env.PORT} [${env.NODE_ENV}]`);
 });
